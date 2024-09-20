5e0e81539e35e1b37e473c7c35fe3fa0fbe5d253
dhruv <djain8834@gmail.com>
2024-09-20 15:17:54 +0530

    added successfully

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./components/AddTodo"
+import Todo from "./components/Todo"
+function App() {
+  return (
+    <>
+      <p>Redux Integration</p>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
